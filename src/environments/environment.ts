455a15fb0c5816802e5b2b3316d0cc2bab7cc2a4
BlaiseTyrel <blaisedaga21@gmail.com>
2024-05-25 23:14:18 +0800

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAAnGNEM1bkEyxCxOMB6JM4T8rHIQHPHbk",
+    authDomain: "ronsted-5debf.firebaseapp.com",
+    projectId: "ronsted-5debf",
+    storageBucket: "ronsted-5debf.appspot.com",
+    messagingSenderId: "350733936773",
+    appId: "1:350733936773:web:c2ab586b548ef7cfe761ec",
+    measurementId: "G-Z0LHQ3GXKH"
+  }
 };
 
 /*
