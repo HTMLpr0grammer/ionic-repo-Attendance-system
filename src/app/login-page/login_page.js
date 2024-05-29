2af669dc3dfbc5188418185055951c14e08dba06
Kentzyann <rubinkentzyann@gmail.com>
2024-05-29 08:16:18 +0800

    update

--- src/app/login-page/login_page.js
+++ src/app/login-page/login_page.js
@@ -11,3 +11,33 @@ window.addEventListener('click', function(event) {
         document.getElementById('recover-popup').style.display = 'none';
     }
 });
+
+function togglePassword(inputId, toggleIcon) {
+      const input = document.getElementById(inputId);
+      if (input.type === "password") {
+        input.type = "text";
+        toggleIcon.classList.replace('bx-hide', 'bx-show');
+      } else {
+        input.type = "password";
+        toggleIcon.classList.replace('bx-show', 'bx-hide');
+      }
+    }
+
+    function hidePasswordIcons() {
+      const icons = document.querySelectorAll('.input-box .visibility-toggle');
+      icons.forEach(icon => {
+        icon.style.display = 'none';
+      });
+    }
+
+    function closePopup() {
+      document.getElementById("recover-popup").style.display = "none";
+      showPasswordIcons();
+    }
+
+    function showPasswordIcons() {
+      const icons = document.querySelectorAll('.input-box .visibility-toggle');
+      icons.forEach(icon => {
+        icon.style.display = 'inline'; // or use 'block' depending on your layout
+      });
+    }
